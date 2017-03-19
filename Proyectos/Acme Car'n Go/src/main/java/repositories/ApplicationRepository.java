7588fceca42375dd63f48045f1f542ea93fcbfbd
Julio <j.troncosocorrea@gmail.com>
2017-03-19 19:37:10 +0100

    Querys en repos

--- Proyectos/Acme Car'n Go/src/main/java/repositories/ApplicationRepository.java
+++ Proyectos/Acme Car'n Go/src/main/java/repositories/ApplicationRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Application;
+import domain.Customer;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
@@ -23,4 +24,10 @@ public interface ApplicationRepository extends JpaRepository<Application, Intege
 
 	@Query("select count(a)*1.0/(select count(p)*1.0 from Post p) from Application a")
 	Double avgApplicationsPerPost();
+
+	@Query("select a.customer from Application a where a.status = 'ACCEPTED' group by a.customer order by count(a) DESC")
+	Collection<Customer> customersWithMoreAccepted();
+
+	@Query("select a.customer from Application a where a.status = 'DENIED' group by a.customer order by count(a) DESC")
+	Collection<Customer> customersWithMoreDenied();
 }

c5336e9f3fd3225aab18d2406048c309fbe82805
Dalmaxor <amartingordillo@gmail.com>
2017-03-28 17:34:41 +0200

    Fin A+

--- Proyectos/Acme Car'n Go/src/test/java/controllers/FolderActorControllerTest.java
+++ Proyectos/Acme Car'n Go/src/test/java/controllers/FolderActorControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -21,7 +20,6 @@ import controllers.actor.FolderActorController;
 @ContextConfiguration(locations = {
 	"classpath:spring/junit.xml"
 })
-@WebAppConfiguration
 public class FolderActorControllerTest extends AbstractTest {
 
 	private MockMvc					mockMvc;
